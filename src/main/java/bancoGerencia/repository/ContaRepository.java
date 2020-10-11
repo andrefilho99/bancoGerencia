d2338a0d0266cfe003b0694fb5c732e11d0be6f1
andre.silva <andre.silva@imd.ufrn.br>
2020-10-11 16:51:39 -0300

    #2 crud de conta criado no service

--- src/main/java/bancoGerencia/repository/ContaRepository.java
+++ src/main/java/bancoGerencia/repository/ContaRepository.java
@@ -9,6 +9,6 @@ import bancoGerencia.model.Conta;
 @Repository
 public interface ContaRepository extends JpaRepository<Conta, Integer>{
 	
-	@Query(value = "select * from conta where id = ?1 and ativo = true", nativeQuery=true)
-	public Conta findOne(Integer id);
+	@Query(value = "select * from conta where num_conta = ?1 and ativo = true", nativeQuery=true)
+	public Conta findOne(String numConta);
 }
